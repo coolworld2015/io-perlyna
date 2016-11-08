9f5dd6083ed9e51ab53d55637ea07a431b88c589
coolworld2015 <wintermute2015@ukr.net>
2016-11-08 12:23:57 +0200

    [general] - refactoring

--- app/src/map/mapCtrl.js
+++ app/src/map/mapCtrl.js
@@ -10,17 +10,40 @@
     function MapCtrl($scope, $state, $ionicLoading) {
         var vm = this;
 		
-
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 15,
+			center: new google.maps.LatLng(49.5443047, 31.8691583),
+			//mapTypeId: google.maps.MapTypeId.ROADMAP
+			mapTypeId: google.maps.MapTypeId.SATELLITE
+		});
 			
         var symbolOne = {
             path: 'M -2,0 0,-2 2,0 0,2 z',
-            strokeColor: 'gold',
-            fillColor: 'gold',
+            strokeColor: 'red',
+            fillColor: 'red',
             fillOpacity: 1,
             scale: 3
         };
+		
+		var goldStar = {
+			path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+			fillColor: 'yellow',
+			fillOpacity: 0.8,
+			scale: 2,
+			strokeColor: 'gold',
+			strokeWeight: 14
+		};
 
-        var marker1;
+		var image = 'no-img.png';
+			
+		var locations = [
+			['Привет Саня - это Перлина Резорт', 49.5443047, 31.8691583, 1],
+			['А это - ЛЕС', 49.5444189, 31.8661804, 2],
+			['А это - ПЛАВНИ', 49.5489871, 31.8649385, 3]
+		];
+		
+		var infowindow = new google.maps.InfoWindow();	
+        var marker, marker1;
 
         angular.extend(vm, {
             showSearch: showSearch,
@@ -41,7 +64,7 @@
 		
 		function doRefresh() {
             init();
-			 $scope.$broadcast('scroll.refreshComplete');
+			$scope.$broadcast('scroll.refreshComplete');
         }
 
         function getPos() {
@@ -64,21 +87,37 @@
             var lat = position.coords.latitude;
             var lng = position.coords.longitude;
 			
-			var map = new google.maps.Map(document.getElementById('map'), {
+			lat = 49.544000;
+			lng = 31.863000;
+			
+			 map = new google.maps.Map(document.getElementById('map'), {
 				zoom: 15,
 				center: new google.maps.LatLng(lat, lng),
-				//mapTypeId: google.maps.MapTypeId.ROADMAP
 				mapTypeId: google.maps.MapTypeId.SATELLITE
 			});
 			
             map.setCenter(new google.maps.LatLng(lat, lng));
+			
             marker1 = new google.maps.Marker({
                 position: new google.maps.LatLng(lat, lng),
                 draggable: true,
-                //animation: google.maps.Animation.BOUNCE,
                 icon: symbolOne,
                 map: map
             });
+			
+			for (var i = 0; i < locations.length; i++) {
+                marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                    map: map
+				});
+ 
+			google.maps.event.addListener(marker, 'click', (function (marker, i) {
+				return function () {
+					infowindow.setContent(locations[i][0]);
+					infowindow.open(map, marker);
+				}
+			})(marker, i));
+			}
         }
 
         function onMapError(error) {
@@ -89,36 +128,14 @@
         function init() {
             vm.searchShowed = true;
             vm.marker1 = '';
-        
-			var map = new google.maps.Map(document.getElementById('map'), {
+						
+			map = new google.maps.Map(document.getElementById('map'), {
 				zoom: 15,
 				center: new google.maps.LatLng(49.5443047, 31.8691583),
-				//mapTypeId: google.maps.MapTypeId.ROADMAP
 				mapTypeId: google.maps.MapTypeId.SATELLITE
 			});
-		
-            var locations = [
-                ['Привет Саня - это Перлина Резорт', 49.5443047, 31.8691583, 1],
-                ['А это - ЛЕС', 49.5444189, 31.8661804, 2],
-                ['А это - ПЛАВНИ', 49.5489871, 31.8649385, 3]
-            ];
-
-            var infowindow = new google.maps.InfoWindow();
-
-            var marker, i;
-
-            var goldStar = {
-                path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-                fillColor: 'yellow',
-                fillOpacity: 0.8,
-                scale: 2,
-                strokeColor: 'gold',
-                strokeWeight: 14
-            };
-
-            var image = 'no-img.png';
-
-            for (i = 0; i < locations.length; i++) {
+		 
+            for (var i = 0; i < locations.length; i++) {
                 marker = new google.maps.Marker({
                     position: new google.maps.LatLng(locations[i][1], locations[i][2]),
                     //icon: {
@@ -141,8 +158,6 @@
                     }
                 })(marker, i));
             }
-
-
         }
     }
 })();
